9ad0496d61738ea90433c574ad9921795c5098a8
carmenz <carmenz@sfu.ca>
2015-11-02 21:24:11 +0000

    trytest3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'cryptic-sea-8286.herokuapp.com'
+  host = 'http://cryptic-sea-8286.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,8 @@ Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
 
+  
+
 
   
 
