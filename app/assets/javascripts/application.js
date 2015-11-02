65b95456a442a100669588513ba21bbdf2942c0f
carmenz <carmenz@sfu.ca>
2015-11-02 20:36:53 +0000

    dropdown

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
+
 //= require jquery
+//= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+
 //= require_tree .
