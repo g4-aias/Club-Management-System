501a002b54c518e7cb9848118c3a500f05434f86
bluebookis <bluebookis@hotmail.com>
2015-11-17 04:23:37 +0000

    my commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,8 @@
 class PostsController < ApplicationController
     
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user,   only: :destroy
     before_action :authorized_member?, only: :new
-    before_action :set_post, only: :destroy
     
     def new
         @club = nil
@@ -41,21 +41,17 @@ class PostsController < ApplicationController
         end
     end
      
-    def destroy #@post instance must be defined , the only way to find the @post is by id we use helper method
-        unless @post.nil?
-            if @post.is_owner?(current_user)
-                @post.destroy
-                flash[:success] = "Post deleted"
-            end
-        end
-        redirect_to root_url
+    def destroy
+        @post.destroy
+        flash[:success] = "Post deleted"
+        redirect_to request.referrer || root_url
     end
 
 
     private
     
     def post_params
-        params.require(:post).permit(:context, :title, :url, :club_id, :picture)
+        params.require(:post).permit(:context, :title, :url, :club_id, :image, :picture)
     end
     
     def authorized_member?
@@ -67,8 +63,9 @@ class PostsController < ApplicationController
         end
     end
 
-    def set_post
-      @post = Post.find(params[:id])
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
     
     
