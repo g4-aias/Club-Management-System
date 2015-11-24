53db9aa543c39b145fb09a7375528b025ec59473
bluebookis <bluebookis@hotmail.com>
2015-11-24 00:44:05 +0000

    making it better

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20151123104737) do
-=======
 ActiveRecord::Schema.define(version: 20151123223627) do
->>>>>>> votes
 
   create_table "clubs", force: :cascade do |t|
     t.string   "name"
