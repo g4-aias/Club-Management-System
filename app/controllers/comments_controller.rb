829b31d84e47fcd0e8cfa56b8c7976a94a3acdf6
bluebookis <bluebookis@hotmail.com>
2015-11-24 20:38:05 +0000

    comment basics done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,40 @@
 class CommentsController < ApplicationController
     before_action :logged_in_user
+    before_action :authorized_member?
     
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(comment_params)
+        @comment = @post.comments.build(comment_params)
         @comment.user = current_user
-    
+        
         if @comment.save
             redirect_to build_post_path(@post)
         else
-            render 'new'
+            redirect_to build_post_path(@post)
         end
+    end
+    
+    def edit
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        
+        if @comment.update(comment_params)
+            redirect_to build_post_path(@post)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to build_post_path(@post)
     end
     
     private
@@ -19,4 +43,14 @@ class CommentsController < ApplicationController
         params.require(:comment).permit(:comment)
     end
     
+    def authorized_member?
+        @post = Post.find(params[:post_id])
+        @club = @post.club
+
+        unless @club.is_member?(current_user)
+            flash[:danger] = "Please become a member of this club to use this function."
+            redirect_to build_post_path(@post)
+        end
+    end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,4 @@
 class Comment < ActiveRecord::Base
-  
   belongs_to :user
   belongs_to :post
-  
 end
