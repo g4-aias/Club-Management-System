c18b3fe9112274b29968813d4fb866f30a9465f2
carmenz <carmenz@sfu.ca>
2015-12-01 06:03:34 +0000

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://safe-stream-8353.herokuapp.com/'
+  host = 'https://blooming-lake-9941.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
