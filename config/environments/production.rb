9df36b783671dbc6fadcfda790da26798414929a
bluebookis <bluebookis@hotmail.com>
2015-12-01 05:49:16 +0000

    making it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://safe-stream-8353.herokuapp.com/'
+  host = 'https://murmuring-springs-5051.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
